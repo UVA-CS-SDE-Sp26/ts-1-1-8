1e84041634b671b25fc86c5c460fd229efdc0623
Genevieve Tierney <tav9zp@virginia.edu>
2026-02-12 19:05:22 -0500

    Added the new test case 4

--- src/main/java/Programcontrol.java
+++ src/main/java/Programcontrol.java
@@ -14,20 +14,20 @@ public class Programcontrol {
         this.handler = handler;
     }
 
-    public String numToFile(int fileNum) {
+    public String numToFile(int fileNum) { //assigns the numbers to the files
         String[] fileList = handler.listFiles();
-        int i = fileNum - 1;
+        int i = fileNum - 1; //must offset by 1
         if (i < 0 || i >= fileList.length) {
             return null;
         }
         return fileList[i];
     }
 
-    public String[] getFileList(){
+    public String[] getFileList(){ //gets the list of files
        return handler.listFiles();
        //listFiles make an array of strings
     }
-    public String fileContents(int fileNum){
+    public String fileContents(int fileNum){ //returns what is in the file
         String fileName = numToFile(fileNum);
         if(fileName ==null){
             return null;
